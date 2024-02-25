21d19709ebdde59c85e29e33331ccf094c37972e
Wellington Migliari <wellingtonmigliari@Wellingtons-MacBook-Air.local>
2024-02-25 19:40:39 +0100

    First commit

---
+++ javascript/footer.js
@@ -0,0 +1,84 @@
+/**
+ * @Author: wellingtonmigliari
+ * @Date:   2022-10-11T17:26:33+02:00
+ * @Last modified by:   wellingtonmigliari
+ * @Last modified time: 2024-02-25T19:39:55+01:00
+ */
+
+footer.innerHTML = `
+<footer>
+   <div class="footer">
+      <div class="container">
+         <div class="row">
+            <div class="col-md-12">
+               <a class="logo_footer" href="index.html"><img src="images/catedra_estudis_barcelona.svg" alt="#"/></a>
+            </div>
+            <div class="col-md-5">
+               <div class="Informa conta">
+                  <h3>adreça</h3>
+                  <ul>
+                     <li>Avinguda Diagonal 649<br>
+                       08028 Barcelona<br>
+                     </li>
+                  </ul>
+               </div>
+               <!--
+               <div class="Informa helpful">
+                  <ul>
+                     <li><a href="index.html">início</a></li>
+                     <li><a href="projeto.html">projeto</a></li>
+                     <li><a href="dados.html">dados</a></li>
+                     <li><a href="equipe.html">mapas</a></li>
+                     <li><a href="estudos.html">estudos</a></li>
+                  </ul>
+               </div>
+             -->
+            </div>
+            <div class="col-md-4">
+               <div class="Informa conta">
+                  <h3>contacte</h3>
+                  <ul>
+                     <li>info@cbeh.cat
+                     </li>
+                  </ul>
+               </div>
+               <ul class="social_icon text_align_center">
+                 <li> <a href="https://github.com/wemigliari/da_cbeh"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+                  <li> <a href="https://twitter.com/CatBCNHabitatge"><i class="fa fa-brands fa-square-x-twitter"></i></a></li>
+               </ul>
+            </div>
+            <div class="col-md-3">
+              <!--
+               <div class="Informa">
+                  <h3>Newsletter</h3>
+                  <form class="newslatter_form">
+
+                     <input class="ente" placeholder="Digite seu e-mail" type="text" name="email" style="color: #c37159;">
+                     <a href="index.html#newsletter"><button class="subs_btn">Inscrever-me</button></a>
+                  </form>
+               </div>
+               -->
+            </div>
+         </div>
+      </div>
+      <div class="copyright text_align_center" style="background-color: #fff;">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-10 offset-md-1">
+                 <div class="Informa helpful">
+                    <ul>
+                       <li><a href="index.html">inici</a></li>
+                       <li><a href="projeto.html">projecte</a></li>
+                       <li><a href="dados.html">dades</a></li>
+                       <li><a href="equipe.html">equip</a></li>
+                       <li><a href="estudos.html">publicacions</a></li>
+                    </ul>
+                 </div>
+                  <p>© 2023-2025 Drets de l'autor <br>Creat per W. Migliari</a></p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</footer>
+`;
