264d9d796ec807a06e337907b61c06b6c0f2c6b6
wemigliari <wemigliari@gmail.com>
2026-06-11 21:58:22 +0200

    First commit

---
+++ javascript/footer.js
@@ -0,0 +1,85 @@
+/**
+ * @Author: wellingtonmigliari
+ * @Date:   2022-10-11T17:26:33+02:00
+ * @Last modified by:   wellingtonmigliari
+ * @Last modified time: 2026-02-13T18:37:29+01:00
+ */
+
+footer.innerHTML = `
+<footer>
+   <div class="footer" style="background-color: #772808;">
+      <div class="container">
+         <div class="row">
+            <div class="col-md-12">
+               <a class="logo_footer" href="index.html"><img src="images/catedra_estudis_barcelona.svg" alt="#"/></a>
+            </div>
+            <div class="col-md-5">
+               <div class="Informa conta">
+                  <h3 style="color: #ec642c;">Dirección</h3>
+                  <ul>
+                     <li>Avinguda Diagonal, 649<br>
+                       Facultat de Dret, Universitat de Barcelona<br>
+                       08028 Barcelona<br>
+                     </li>
+                  </ul>
+               </div>
+               <!--
+               <div class="Informa helpful">
+                  <ul>
+                     <li><a href="index.html">início</a></li>
+                     <li><a href="projeto.html">projeto</a></li>
+                     <li><a href="dados.html">dados</a></li>
+                     <li><a href="equipe.html">mapas</a></li>
+                     <li><a href="estudos.html">estudos</a></li>
+                  </ul>
+               </div>
+             -->
+            </div>
+            <div class="col-md-4">
+               <div class="Informa conta">
+                  <h3 style="color: #ec642c;">Contacto</h3>
+                  <ul>
+                     <li>info@cbeh.cat
+                     </li>
+                  </ul>
+               </div>
+               <ul class="social_icon text_align_center">
+                 <li> <a href="https://github.com/wemigliari/da_cbeh" target="blank"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+                  <li> <a href="https://www.linkedin.com/in/c%C3%A0tedra-barcelona-d-estudis-d-habitatge-11623a233/" target="blank"><i class="fa fa-brands fa-linkedin"></i></a></li>
+               </ul>
+            </div>
+            <div class="col-md-3">
+              <!--
+               <div class="Informa">
+                  <h3>Newsletter</h3>
+                  <form class="newslatter_form">
+
+                     <input class="ente" placeholder="Digite seu e-mail" type="text" name="email" style="color: #c37159;">
+                     <a href="index.html#newsletter"><button class="subs_btn">Inscrever-me</button></a>
+                  </form>
+               </div>
+               -->
+            </div>
+         </div>
+      </div>
+      <div class="copyright text_align_center" style="background-color: #772808;">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-10 offset-md-1">
+                 <div class="Informa helpful">
+                    <ul>
+                       <li><a href="index.html">home</a></li>
+                       <li><a href="projeto.html">proyecto</a></li>
+                       <li><a href="dados.html">datos</a></li>
+                       <li><a href="equipe.html">equipo</a></li>
+                       <li><a href="estudos.html">publicaciones</a></li>
+                    </ul>
+                 </div>
+                  <p>© 2025-2026 Derechos del autor <br>Creado por W. Migliari</a></p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</footer>
+`;
